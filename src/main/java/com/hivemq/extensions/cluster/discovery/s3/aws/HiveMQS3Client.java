4984508757c79a6fddf8a207838eb2610fa1a5e7
yannick <yannick.weber@hivemq.com>
2023-01-18 13:16:50 +0100

    correctly close s3Client in HiveMQS3Client version to 4.1.1

--- src/main/java/com/hivemq/extensions/cluster/discovery/s3/aws/HiveMQS3Client.java
+++ src/main/java/com/hivemq/extensions/cluster/discovery/s3/aws/HiveMQS3Client.java
@@ -68,6 +68,9 @@ public class HiveMQS3Client {
         if (newS3Config == null) {
             throw new IllegalStateException("Configuration of the S3 discovery extension couldn't be loaded.");
         }
+        if (s3Config.equals(newS3Config)) {
+            return;
+        }
         s3Config = newS3Config;
         LOG.trace("{}: Loaded configuration successfully.", EXTENSION_NAME);
         final AuthenticationType authenticationType = AuthenticationType.fromName(s3Config.getAuthenticationTypeName());
@@ -89,6 +92,7 @@ public class HiveMQS3Client {
         if (s3Config.getPathStyleAccess() != null) {
             s3ConfigurationBuilder.pathStyleAccessEnabled(s3Config.getPathStyleAccess());
         }
+        s3Client.close();
         s3Client = s3ClientBuilder.credentialsProvider(credentialsProvider)
                 .serviceConfiguration(s3ConfigurationBuilder.build())
                 .build();
